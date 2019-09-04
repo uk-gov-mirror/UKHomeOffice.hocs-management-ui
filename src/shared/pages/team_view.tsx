d2fe3dcab1113e95f16d35fdbd128919d5dc333b
Leigh Kirk <leigh.kirk@digital.homeoffice.gov.uk>
2019-09-04 14:30:18 +0100

    Add team_view component

---
+++ src/shared/pages/team_view.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TeamView : React.FC = () => {
+
+    return (
+        <div className="govuk-form-group">
+            <h1 className="govuk-heading-xl">
+                Team
+            </h1>
+        </div>
+    );
+};
+
+export default TeamView;
