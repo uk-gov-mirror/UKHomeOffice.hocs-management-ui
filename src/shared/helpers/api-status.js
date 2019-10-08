777646251ced3c5c67e9dad3b7f5c846755bafe3
Leigh Kirk <leigh.kirk@digital.homeoffice.gov.uk>
2019-10-08 15:05:48 +0100

    Refactor topicSearch component

--- src/shared/helpers/api-status.js
+++ src/shared/helpers/api-status.js
@@ -7,6 +7,9 @@ const status = {
     REQUEST_PARENT_TOPICS: { display: 'Requesting parent topics', level: 1, type: 'OK', timeoutPeriod: timeout.STANDARD },
     REQUEST_PARENT_TOPICS_SUCCESS: { display: 'parent topics received', level: 3, type: 'OK', timeoutPeriod: timeout.STANDARD },
     REQUEST_PARENT_TOPICS_FAILURE: { display: 'Unable to fetch parent topics', level: 0, type: 'ERROR', timeoutPeriod: timeout.ERROR },
+    REQUEST_TOPICS: { display: 'Requesting topics', level: 1, type: 'OK', timeoutPeriod: timeout.STANDARD },
+    REQUEST_TOPICS_SUCCESS: { display: 'topics received', level: 3, type: 'OK', timeoutPeriod: timeout.STANDARD },
+    REQUEST_TOPICS_FAILURE: { display: 'Unable to fetch topics', level: 0, type: 'ERROR', timeoutPeriod: timeout.ERROR },
 };
 
 export default status;
--- src/shared/pages/topic/topicSearch/topicSearch.tsx
+++ src/shared/pages/topic/topicSearch/topicSearch.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, Reducer } from 'react';
+import React, { Reducer, useCallback } from 'react';
+import { RouteComponentProps } from 'react-router';
 import TypeAhead from '../../../common/components/typeAhead';
 import { getTopics } from '../../../services/topicsService';
 import { History } from 'history';
@@ -14,33 +15,41 @@ import {
     GENERAL_ERROR_TITLE,
     LOAD_TOPICS_ERROR_DESCRIPTION
 } from '../../../models/constants';
-import Topic from '../../../models/topic';
 import ErrorMessage from "../../../models/errorMessage";
+import {Link} from "react-router-dom";
+import Submit from "../../../common/components/forms/submit";
+import {ContextAction, updateApiStatus} from "../../../contexts/actions";
+import Item from "../../../models/item";
+import status from "../../../helpers/api-status";
 
-interface TopicSearchProps {
+interface TopicSearchProps extends RouteComponentProps {
+    csrfToken?: string;
+    contextDispatch: (action: ContextAction<any>) => Promise<any>;
     history: History;
 }
 
-const TopicSearch: React.FC<TopicSearchProps> = ({ history }) => {
+const TopicSearch: React.FC<TopicSearchProps> = ({ csrfToken, contextDispatch, history }) => {
 
     const [pageError, , , setErrorMessage] = useError();
     const [state, dispatch] = React.useReducer<Reducer<State, Action>>(reducer, initialState);
 
-
-
-    useEffect(() => {
+    const getTopicsForTypeahead = useCallback(() => new Promise<Item[]>((resolve) => {
+        contextDispatch(updateApiStatus(status.REQUEST_TOPICS));
         getTopics()
-            .then((topics: Topic[]) => {
-                dispatch({ type: 'SetTopics', payload: topics }); })
+            .then((parentTopics: Item[]) => {
+                contextDispatch(updateApiStatus(status.REQUEST_TOPICS_SUCCESS));
+                resolve(parentTopics);
+            })
             .catch(() => {
-                dispatch({ type: 'SetGeneralError', payload: { description: LOAD_TOPICS_ERROR_DESCRIPTION, title: GENERAL_ERROR_TITLE }
-                });
+                contextDispatch(updateApiStatus(status.REQUEST_TOPICS_FAILURE));
+                setErrorMessage(new ErrorMessage(LOAD_TOPICS_ERROR_DESCRIPTION, GENERAL_ERROR_TITLE));
+                resolve([]);
             });
-    }, []);
+    }), []);
 
-    const onSelectedTopicChange = (selectedTopic: any) => {
+    const onSelectedTopicChange = useCallback((selectedTopic: Item) => {
         dispatch({ type: 'SetTopicValue', payload: selectedTopic.value });
-    };
+    }, []);
 
     const handleOnSubmit = () => {
         if (state.topicValue === '') {
@@ -50,40 +59,36 @@ const TopicSearch: React.FC<TopicSearchProps> = ({ history }) => {
         }
     };
 
-    const onBackLinkClick = (history: History) => {
-        history.push('/');
-    };
-
     return (
-        <div className="govuk-form-group">
-            <a href="" onClick={() => onBackLinkClick(history)} className="govuk-back-link">Back</a>
-            <ErrorSummary
-                pageError={pageError}
-            />
-            <h1 className="govuk-heading-xl">
-                Topic search
-            </h1>
-            {
-                state.topicsLoaded ?
-                    <div>
-                        state topics
-                        {state.topics}
+        <>
+            <div className="govuk-grid-row">
+                <div className="govuk-grid-column-two-thirds-from-desktop">
+                    <Link to="/" className="govuk-back-link">Back</Link>
+                    <ErrorSummary
+                        pageError={pageError}
+                    />
+                    <h1 className="govuk-heading-xl">
+                        Topic search
+                    </h1>
+                </div>
+            </div>
+            <div className="govuk-grid-row">
+                <div className="govuk-grid-column-one-half-from-desktop">
+                    <form method="POST" onSubmit={handleOnSubmit}>
+                        <input type="hidden" name="_csrf" value={csrfToken} />
                         <TypeAhead
-                            choices={[ {label: 'one', value: 'one'}]}
                             clearable={true}
                             disabled={false}
+                            getOptions={getTopicsForTypeahead}
                             label={'Topics'}
-                            name={'Topics'}
+                            name={'topics'}
                             onSelectedItemChange={onSelectedTopicChange}
-                        ></TypeAhead>
-                    </div> :
-                    <div>
-                        ...loading
-                    </div>
-            }
-
-            <button type="submit" className="govuk-button view-team-button" onClick={() => { handleOnSubmit(); }}>View linked teams</button>
-        </div>
+                        />
+                        <Submit />
+                    </form>
+                </div>
+            </div>
+        </>
     );
 };
 
