ab0d0c62c77fe067473dcae748fba19f0c56d0b9
Leigh Kirk <leigh.kirk@digital.homeoffice.gov.uk>
2019-10-24 20:17:06 +0100

    HOCS-1205 Build serverside template code

--- server/middleware/template.js
+++ server/middleware/template.js
@@ -1,27 +1,28 @@
-const { infoService } = require('../clients/index');
-const getLogger = require('../libs/logger');
 const User = require('../models/user');
-
+const getLogger = require('../libs/logger');
+const { infoService } = require('../clients/index');
 
 async function addTemplate(req, res, next) {
-
-    const logger = getLogger(req.request);
-
-    //req.body should include strings
-    //@JsonProperty("displayName")
-    //@JsonProperty("caseType")
-    //@JsonProperty("s3UntrustedUrl")
-
+    let logger;
     try {
-        await infoService.post(`/template`, req.body, { headers: User.createHeaders(req.user) });
+        logger = getLogger(req.request);
+        const document = req.files[0];
+        const request = {
+            s3UntrustedUrl: document.key,
+            displayName: document.originalname,
+            caseType: req.body.caseType,
+        };
+
+        const options = {
+            headers: User.createHeaders(req.user)
+        };
+        await infoService.post('/template', request, options);
         res.sendStatus(200);
-    } catch (error) {
-        logger.error(error);
+    }
+    catch (error) {
+        logger.error(error.message);
         next(error);
     }
 }
 
-module.exports = {
-    addTemplate
-};
-
+module.exports = addTemplate;
