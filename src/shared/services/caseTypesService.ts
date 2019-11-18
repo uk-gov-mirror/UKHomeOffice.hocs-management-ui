3986f351252e2d92320ef9a81331db3dcf2f3946
Lewis Muirhead <lewis.muirhead@bjss.com>
2019-11-18 19:00:48 +0000

    HOCS-1311 service for templates by casetype

--- src/shared/services/caseTypesService.ts
+++ src/shared/services/caseTypesService.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import CaseType from '../models/caseType';
 import Item from '../models/item';
+import TemplateMeta from '../models/templateMeta';
 
 const sortByLabel: ((a: Item, b: Item) => number) = (a, b) => {
     const labelA = a.label.toLocaleLowerCase();
@@ -18,6 +19,14 @@ export const getCaseTypes = () => new Promise<CaseType[]>((resolve, reject) =>
     axios.get('/api/case-types')
         .then((response) => {
             resolve(response.data.sort(sortByLabel));
+        })
+        .catch(error => reject(error))
+);
+
+export const getTemplatesForCaseType = (caseTypeId: string) => new Promise<Item[]>((resolve, reject) =>
+    axios.get<TemplateMeta[]>(`/api/case-types/${caseTypeId}/templates`)
+        .then((response) => {
+            resolve(response.data.map(({ displayName, uuid }) => ({ label: displayName, value: uuid })).sort(sortByLabel));
         })
         .catch(error => reject(error))
 );
