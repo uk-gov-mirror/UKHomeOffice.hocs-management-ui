f5eabbe1f20b14f0b1dd09fcd0513255ef6490db
Lewis Muirhead <lewis.muirhead@bjss.com>
2019-11-18 18:19:07 +0000

    added handler for get templates by casetype

--- server/middleware/template.js
+++ server/middleware/template.js
@@ -3,9 +3,8 @@ const getLogger = require('../libs/logger');
 const { infoService } = require('../clients/index');
 
 async function addTemplate(req, res, next) {
-    let logger;
+    const logger = getLogger(req.request);
     try {
-        logger = getLogger(req.request);
         const document = req.files[0];
         const request = {
             s3UntrustedUrl: document.key,
@@ -25,4 +24,21 @@ async function addTemplate(req, res, next) {
     }
 }
 
-module.exports = addTemplate;
+
+async function getTemplatesForCaseType(req, res, next) {
+    const logger = getLogger(req.request);
+    try {
+        const { caseType } = req.params;
+        const response = await infoService.get(`/caseType/${caseType}/templates`, { headers: User.createHeaders(req.user) });
+        res.json(response.data);
+    } catch (error) {
+        logger.error(error.message);
+        next(error);
+    }
+}
+
+
+module.exports = {
+    addTemplate,
+    getTemplatesForCaseType
+};
--- server/routes/api/caseTypes.js
+++ server/routes/api/caseTypes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { getCaseTypes, returnCaseTypesJson } = require('../../middleware/caseType');
+const { getTemplatesForCaseType } = require('../../middleware/template');
 
 router.get('', getCaseTypes, returnCaseTypesJson);
+
+router.get('/:caseTypeId/templates', getTemplatesForCaseType);
 
 module.exports = router;
