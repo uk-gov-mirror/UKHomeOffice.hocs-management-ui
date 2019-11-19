aebcfb92bcf394824a14d226b3113c3a5d3fc40d
Lewis Muirhead <lewis.muirhead@bjss.com>
2019-11-19 13:58:01 +0000

    HOCS-1311 added tests for the caseTypeView page

--- src/shared/models/caseType.ts
+++ src/shared/models/caseType.ts
@@ -1,7 +1,7 @@
 export default interface CaseType {
-    label: string;
-    value: string;
     displayName: string;
+    label: string;
     shortCode: string;
     type: string;
+    value: string;
 }
