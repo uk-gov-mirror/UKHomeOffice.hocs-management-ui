2ea1064d76f31910c6c031fadc79843cb0bfdcd0
Lewis Muirhead <lewis.muirhead@bjss.com>
2019-11-19 15:10:28 +0000

    HOCS-1311 changed the add template page to use supplied case type

--- src/shared/pages/template/addTemplate.tsx
+++ src/shared/pages/template/addTemplate.tsx
@@ -1,67 +1,55 @@
-import React, { Reducer, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import { RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom';
-import { object, string, array } from 'yup';
+import { array } from 'yup';
 import Submit from '../../common/components/forms/submit';
 import { ApplicationConsumer } from '../../contexts/application';
-import { reducer } from './reducer';
 import ErrorSummary from '../../common/components/errorSummary';
 import useError from '../../hooks/useError';
 import * as constants from '../../models/constants';
 import ErrorMessage from '../../models/errorMessage';
-import InputEventData from '../../models/inputEventData';
 import { validate } from '../../validation';
 import DocumentAdd from '../../common/components/forms/documentAdd';
-import TypeAhead from '../../common/components/typeAhead';
-import Item from '../../models/item';
-import Template from '../../models/template';
 import { addTemplate } from '../../services/templatesService';
-import { getCaseTypes } from '../../services/caseTypesService';
+import { getCaseType } from '../../services/caseTypesService';
+import CaseType from 'shared/models/caseType';
 
-interface AddTemplateProps extends RouteComponentProps {
+interface MatchParams {
+    type: string;
+}
+interface AddTemplateProps extends RouteComponentProps<MatchParams> {
     csrfToken?: string;
 }
 
-const validationSchema = object({
-    files: array()
-        .max(1)
-        .min(1)
-        .required()
-        .label('Template'),
-    caseType: string()
-        .required()
-        .label('Case Types')
-});
+const validationSchema = array()
+    .max(1)
+    .min(1)
+    .required()
+    .label('Template');
 
-const AddTemplate: React.FC<AddTemplateProps> = ({ csrfToken, history }) => {
-
+const AddTemplate: React.FC<AddTemplateProps> = ({ csrfToken, history, match }) => {
+    const { params: { type } } = match;
     const [pageError, addFormError, clearErrors, setErrorMessage] = useError('', constants.VALIDATION_ERROR_TITLE);
 
-    const [template, dispatch] = React.useReducer<Reducer<Template, InputEventData>>(reducer, {});
+    const [caseType, setCaseType] = React.useState<CaseType>();
+    const [files, setFiles] = React.useState<File[]>();
 
-    const getCaseTypesForTypeahead = useCallback(() => new Promise<Item[]>((resolve) => {
-        getCaseTypes()
-            .then((caseTypes: Item[]) => {
-                resolve(caseTypes);
-            })
+    useEffect(() => {
+        getCaseType(type)
+            .then(setCaseType)
             .catch(() => {
-                setErrorMessage(new ErrorMessage(constants.LOAD_CASE_TYPES_DESCRIPTION, constants.GENERAL_ERROR_TITLE));
-                resolve([]);
+                setErrorMessage(new ErrorMessage(constants.LOAD_CASE_TYPE_ERROR_DESCRIPTION, constants.GENERAL_ERROR_TITLE));
             });
-    }), []);
-
-    const onSelectedCaseTypeChange = useCallback((selectedCaseType: Item) => {
-        dispatch({ name: 'caseType', value: selectedCaseType });
     }, []);
 
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault();
         clearErrors();
-        if (validate(validationSchema, template, addFormError)) {
+        if (validate(validationSchema, files, addFormError) && caseType) {
 
             const data = new FormData();
-            data.append('file', template.files![0]);
-            data.append('caseType', template.caseType!.value);
+            data.append('file', files![0]);
+            data.append('caseType', caseType.value);
 
             addTemplate(data).then(() => {
                 history.push('/', { successMessage: constants.ADD_TEMPLATE_SUCCESS });
@@ -75,29 +63,23 @@ const AddTemplate: React.FC<AddTemplateProps> = ({ csrfToken, history }) => {
         <>
             <div className="govuk-grid-row">
                 <div className="govuk-grid-column-two-thirds-from-desktop">
-                    <Link to="/" className="govuk-back-link">Back</Link>
+                    <Link to={`/case-type/${type}`} className="govuk-back-link">Back</Link>
                     <ErrorSummary pageError={pageError} />
                     <h1 className="govuk-heading-xl">
                         Add a Template
                     </h1>
+                    <h2 className="govuk-heading-l">
+                        {caseType && `Case Type: ${caseType.displayName}`}
+                    </h2>
                 </div>
             </div>
             <div className="govuk-grid-row">
                 <div className="govuk-grid-column-one-half-from-desktop">
                     <form action="/api/template" method="POST" onSubmit={handleSubmit}>
                         <input type="hidden" name="_csrf" value={csrfToken} />
-                        <TypeAhead
-                            clearable={true}
-                            disabled={false}
-                            getOptions={getCaseTypesForTypeahead}
-                            label={'Case Types'}
-                            name={'case-types'}
-                            onSelectedItemChange={onSelectedCaseTypeChange}
-                            value={template.caseType}
-                        />
                         <DocumentAdd
                             name="files"
-                            updateState={dispatch}
+                            updateState={documentAddState => setFiles(documentAddState.value as File[])}
                         />
                         <Submit />
                     </form>
@@ -107,7 +89,7 @@ const AddTemplate: React.FC<AddTemplateProps> = ({ csrfToken, history }) => {
     );
 };
 
-const WrappedAddTemplate = ({ history, location, match }: RouteComponentProps) => (
+const WrappedAddTemplate = ({ history, location, match }: RouteComponentProps<MatchParams>) => (
     <ApplicationConsumer>
         {({ csrf }) => (
             <AddTemplate csrfToken={csrf} history={history} location={location} match={match} />
