f327183e6472929c39b54f9a4182dd37c99a3916
raysonviswas <rayson.viswas@live.co.uk>
2020-03-10 11:25:15 +0000

    added middleware tests

--- server/middleware/__tests__/topic.spec.js
+++ server/middleware/__tests__/topic.spec.js
@@ -1,7 +1,7 @@
 import { infoService } from '../../clients/index';
 import getLogger from '../../libs/logger';
 import { createHeaders } from '../../models/user';
-import { addTopic, getParentTopics, returnParentTopicsJson, getTopics, returnTopicsJson, getTopic, returnTopicJson, addDCUTeamsToTopic } from '../topic';
+import { addTopic, addParentTopic, getParentTopics, returnParentTopicsJson, getTopics, returnTopicsJson, getTopic, returnTopicJson, addDCUTeamsToTopic } from '../topic';
 
 jest.mock('../../clients/index');
 jest.mock('../../libs/logger');
@@ -278,6 +278,51 @@ describe('Topic middleware addTopic', () => {
         const logError = jest.fn();
         getLogger.mockImplementation(() => ({ error: logError }));
         await addTopic(req, res, next);
+        expect(logError).toHaveBeenCalled();
+        expect(next).toHaveBeenCalledWith('__error__');
+    });
+});
+
+fdescribe('Topic middleware addParentTopic', () => {
+
+    const headers = '__headers__';
+    const topicToCreate = { displayName: '__displayName__' };
+    const req = { body: topicToCreate, user: '__user__' };
+    const sendStatus = jest.fn();
+    const res = { sendStatus };
+    const next = jest.fn();
+
+    beforeEach(() => {
+        next.mockReset();
+        sendStatus.mockReset();
+    });
+
+    it('should call the post method on the info service', async () => {
+        createHeaders.mockImplementation(() => headers);
+        await addParentTopic(req, res, next);
+        expect(infoService.post).toHaveBeenCalledWith('/topic/parent', topicToCreate, { headers: headers });
+    });
+
+    it('should call the user create headers method', async () => {
+        await addParentTopic(req, res, next);
+        expect(createHeaders).toHaveBeenCalled();
+    });
+
+    it('should call the sendstatus method with a success code', async () => {
+        await addParentTopic(req, res, next);
+        expect(sendStatus).toHaveBeenCalledWith(200);
+    });
+
+    it('should get the logger instance', async () => {
+        await addParentTopic(req, res, next);
+        expect(getLogger).toHaveBeenCalled();
+    });
+
+    it('should log when the request fails', async () => {
+        infoService.post.mockImplementation(() => Promise.reject('__error__'));
+        const logError = jest.fn();
+        getLogger.mockImplementation(() => ({ error: logError }));
+        await addParentTopic(req, res, next);
         expect(logError).toHaveBeenCalled();
         expect(next).toHaveBeenCalledWith('__error__');
     });
