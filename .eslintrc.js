23a4583810b3f09592073f4ef1f4cda5ac954ec1
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-01-29 14:14:30 +0000

    HOCS-2381: Upgrade eslint to handle typescript
    As a result of tslint being out of date and eslint now allowing for
    testing of typescript files, this commit brings eslint upto the latest
    release.
    Previously we weren't running linting on .tsx files, this commit also
    also adds linting to .tsx.
    Remove the tslint dependencies as they are no longer required.
    Add .idea folder to the .eslintignore.
    ddee

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,63 @@
 module.exports = {
-    'env': {
-        'es6': true,
-        'node': true
+    env: {
+        es6: true,
+        node: true
     },
-    'extends': [
-        'airbnb',
-        'eslint:recommended',
-        'plugin:react/recommended'
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+    },
+    extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended'
     ],
-    'parser': 'babel-eslint',
-    'parserOptions': {
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'rules': {
-        'react/forbid-prop-types': false,
-        'import/extensions': false,
-        'ter-indent': [
-            true,
-            4,
-            { 'SwitchCase': 1 }
+    rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off', // TODO: This needs reviewing
+        '@typescript-eslint/no-explicit-any': 'off', // TODO: This need reviewing
+        '@typescript-eslint/no-var-requires': 'off', // TODO: This need reviewing
+        '@typescript-eslint/no-empty-function': 'off', // TODO: This needs reviewing
+        '@typescript-eslint/no-unused-vars': 'off', // TODO: This needs reviewing
+        '@typescript-eslint/no-non-null-assertion': 'off', // TODO: This needs reviewing
+        '@typescript-eslint/ban-ts-comment': 'off', // TODO: This needs reviewing
+        'react/prop-types': 'off', // TODO: This needs reviewing
+        'react/no-children-prop': 'off', // TODO: This needs reviewing
+        'indent': [
+            'error',
+            4
+            , { 'SwitchCase': 1 }
         ],
-        'react/jsx-indent': 4
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        // TODO: this needs looking at.
+        'no-unused-vars': 'off',
+        /*'no-unused-vars': [
+            'error',
+            { 'argsIgnorePattern': '^_$'  }
+        ],*/
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'no-trailing-spaces': [
+            'error'
+        ]
     },
-    'overrides': [
+    overrides: [
         {
-            'files': ['**/__tests__/**'],
-            'env': {
-                'jest': true
-            },
-            'globals': {
-                'render': false,
-                'shallow': false,
-                'wrapper': false,
-                'mount': false
-            }
-        },
-        {
-            'files': ['**/__snapshots__/**'],
-            'excludedFiles': '*.js.snap'
-        },
-        {
-            'files': ['src/**'],
-            'env': {
-                'commonjs': true
+            'files': ['*.ts', '*.tsx'],
+            'rules': {
+                '@typescript-eslint/semi': ['error'],
+                'semi': 'off'
             }
         }
     ]
