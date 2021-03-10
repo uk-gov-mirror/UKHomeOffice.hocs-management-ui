b1cd4160b71bf9548240ac424fda22c9a507efe4
Parth Vyas <parth.vyas@digital.homeoffice.gov.uk>
2021-03-10 16:47:48 +0000

    HOCS-2502: Create new route for create team page.

--- src/shared/router/routes/index.ts
+++ src/shared/router/routes/index.ts
@@ -24,6 +24,7 @@ import WithdrawCase from '../../pages/case/withdrawCase';
 import CampaignsView from '../../pages/list/mpamCampaign/campaignsView';
 import AddCampaign from '../../pages/list/mpamCampaign/addCampaign';
 import AmendCampaign from '../../pages/list/mpamCampaign/amendCampaign';
+import CreateTeam from '../../pages/team/createTeam/createTeam';
 
 export interface Route {
     component: React.FunctionComponent | Error;
@@ -62,6 +63,12 @@ const routes = [
         exact: true,
         component: TeamSearch,
         title: 'TeamSearch'
+    },
+    {
+        path: '/create-team',
+        exact: true,
+        component: CreateTeam,
+        title: 'CreateTeam'
     },
     {
         path: '/team-view/:teamId',
