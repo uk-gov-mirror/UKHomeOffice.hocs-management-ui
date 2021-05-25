5d5b7713d12e0eb6aa24544c640741269daf1000
Douglas Gardner <douglas.gardner@digital.homeoffice.gov.uk>
2021-05-25 15:30:15 +0100

    HOCS-3061 Verify TLS cert before deploying

--- deploy.sh
+++ deploy.sh
@@ -16,12 +16,14 @@ then
     export MAX_REPLICAS="6"
     export KUBE_SERVER=https://kube-api-prod.prod.acp.homeoffice.gov.uk
     export UPTIME_PERIOD="Mon-Sun 05:00-23:00 Europe/London"
+    export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-prod.crt"
 else
     export MIN_REPLICAS="1"
     export MAX_REPLICAS="2"
     export KUBE_SERVER=https://kube-api-notprod.notprod.acp.homeoffice.gov.uk
     export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
     export UPTIME_PERIOD="Mon-Fri 08:00-18:00 Europe/London"
+    export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt"
 fi
 
 if [[ "${KUBE_NAMESPACE}" == "wcs-prod" ]] ; then
@@ -69,8 +71,7 @@ echo
 
 cd kd || exit 1
 
-kd --insecure-skip-tls-verify \
-   --timeout 10m \
+kd --timeout 10m \
     -f ingress-${INGRESS_TYPE}.yaml \
     -f deployment.yaml \
     -f service.yaml \
