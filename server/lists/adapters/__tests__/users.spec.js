53a61c56e5d03668b0a4c251d7d639838a5333c7
Laurie Bennett <84858704+LaurieBennett@users.noreply.github.com>
2021-06-22 11:24:58 +0100

    HOCS-2272 Missing forename/surname of users in MUI shows up as "null" (#101)
    * hocs-2272 initial commit updated code to ignore null values when displaying names and emails
    
    * hocs-2272 added addition checks for the null value test to cover more possibilities

--- server/lists/adapters/__tests__/users.spec.js
+++ server/lists/adapters/__tests__/users.spec.js
@@ -21,4 +21,33 @@ describe('User Adapter', () => {
         expect(results).toBeDefined();
         expect(results).toMatchSnapshot();
     });
+
+    it('should correctly transform null values', async () => {
+        const mockData = [
+            { username: 'userA', firstName: null, lastName: 'A', email: 'user.a@example.com', id: 1 },
+            { username: 'userB', firstName: 'User', lastName: null, email: 'user.b@example.com', id: 2 },
+            { username: 'userC', firstName: 'User', lastName: 'C', email: null, id: 3 },
+            { username: 'userD', firstName: null, lastName: 'D', email: null, id: 4 },
+            { username: 'userE', firstName: 'User', lastName: null, email: null, id: 5 },
+            { username: 'userF', firstName: null, lastName: null, email: 'user.F@example.com', id: 6 },
+            { username: 'userG', firstName: null, lastName: null, email: null, id: 7 },
+            { username: 'userH', firstName: 'null', lastName: 'Null', email: 'null.Null@example.com', id: 8 }
+        ];
+
+        // Use an id of 0 here to fetch all of the users
+        const results = await usersAdapter(mockData, { user: { id: 0 }, logger: mockLogger });
+        results.sort((a, b) => {
+            return a.value - b.value;
+        });
+        expect(results).toBeDefined();
+        expect(results).toMatchSnapshot();
+        expect(results[0].label).toEqual('A (user.a@example.com)');
+        expect(results[1].label).toEqual('User (user.b@example.com)');
+        expect(results[2].label).toEqual('User C (userC)');
+        expect(results[3].label).toEqual('D (userD)');
+        expect(results[4].label).toEqual('User (userE)');
+        expect(results[5].label).toEqual('user.F@example.com');
+        expect(results[6].label).toEqual('userG');
+        expect(results[7].label).toEqual('null Null (null.Null@example.com)');
+    });
 });
--- server/lists/adapters/users.js
+++ server/lists/adapters/users.js
@@ -4,8 +4,13 @@ module.exports = async (data, { user, logger }) => {
     logger.debug('REQUEST_USERS', { users: data.length });
     return data
         .filter(u => u.id !== user.id)
-        .map(({ id, firstName, lastName, email }) => ({
-            label: `${firstName} ${lastName} (${email})`,
+        // When we do not have a firstName, or lastName we display an empty String, when we do not have an email we display the username,
+        // when we only have an email/username we display it without brackets.
+        .map(({ id, username, firstName, lastName, email }) => ({
+            label: `${((firstName === null) ? '' : firstName + ' ')}` +
+            `${((lastName === null) ? '' : lastName + ' ')}` +
+            `${((email === null) ? (firstName === null && lastName === null) ? username : '(' + username + ')'
+                : (firstName === null && lastName === null) ? email :  '(' + email + ')')}`,
             value: id
         }))
         .sort(byLabel);
