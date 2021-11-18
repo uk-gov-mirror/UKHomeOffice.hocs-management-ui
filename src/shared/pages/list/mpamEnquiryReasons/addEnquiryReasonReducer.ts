565fb5998e6db2d05b16790518c252d2303c8deb
Joseph Rogerson <92527240+JosephRogerson@users.noreply.github.com>
2021-11-18 16:54:47 +0000

    HOCS-3639: Set simpleName in addEnquiryReasons (#135)
    Currently, the simpleName is not being set when requests are sent to
    add enquiry reasons via the MUI. This makes the new reasons all detect
    as duplicate and makes them unselectable in the frontend. This change
    adds this value to requests sent this way.

--- src/shared/pages/list/mpamEnquiryReasons/addEnquiryReasonReducer.ts
+++ src/shared/pages/list/mpamEnquiryReasons/addEnquiryReasonReducer.ts
@@ -2,6 +2,6 @@ import EntityListItem from '../../../models/entityListItem';
 import InputEventData from '../../../models/inputEventData';
 
 export const reducer = (state: EntityListItem, inputEventData: InputEventData) => {
-    const newState = { ...state, [inputEventData.name]: inputEventData.value };
+    const newState = { ...state, [inputEventData.name]: inputEventData.value, simpleName: inputEventData.value.toString() };
     return newState;
 };
