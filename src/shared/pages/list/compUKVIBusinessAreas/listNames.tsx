0a3790b31955c85324a36d652bd44d2a82028727
vinothon84 <vinothon84@gmail.com>
2023-10-03 15:18:34 +0100

    HOCS-6791:Add UKVI COMP new directorate and business areas

--- src/shared/pages/list/compUKVIBusinessAreas/listNames.tsx
+++ src/shared/pages/list/compUKVIBusinessAreas/listNames.tsx
@@ -1,7 +1,10 @@
 export const listNames = {
-    COMP_IANDP_BUS_AREA: 'IandP',
-    COMP_RASI_BUS_AREA: 'RASI',
-    COMP_VCIC_BUS_AREA: 'VCIC',
-    COMP_VCOS_BUS_AREA: 'VCOS',
+    COMP_ASYLUM_AND_HUMAN_RIGHTS_BUS_AREA: 'Asylum and Human Rights',
+    COMP_ASYLUM_AND_SUPPORT_BUS_AREA: 'Asylum Support',
+    COMP_COSS_BUS_AREA:  'COSS',
+    COMP_IMMIGRATION_ENFORCEMENT_BUS_AREA:  'Immigration Enforcement',
     COMP_OTHER_BUS_AREA: 'Other',
-    COMP_EUSS_FP_BUS_AREA: 'EUSS FP' };
+    COMP_PCCR_BUS_AREA: 'PCCR',
+    COMP_RESETTLEMENT_BUS_AREA: 'Resettlement',
+    COMP_VSI_BUS_AREA: 'VSI'
+};
